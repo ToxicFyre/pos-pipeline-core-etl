ea5c34563b26ca9af9358f66f6ebab3418e9af34
ToxicFyre <mzenkl@hotmail.com>
2025-11-28 19:00:08 -0600

    Add NaiveLastValueModel to forecasting models
    Introduces NaiveLastValueModel, a simple baseline forecasting model that repeats the last observed value for future predictions. Updates the models package to include this new model in the public API.

--- src/pos_core/forecasting/models/__init__.py
+++ src/pos_core/forecasting/models/__init__.py
@@ -2,5 +2,6 @@
 
 from pos_core.forecasting.models.arima import LogARIMAModel
 from pos_core.forecasting.models.base import ForecastModel
+from pos_core.forecasting.models.naive import NaiveLastValueModel
 
-__all__ = ["ForecastModel", "LogARIMAModel"]
+__all__ = ["ForecastModel", "LogARIMAModel", "NaiveLastValueModel"]
